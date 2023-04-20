5119d023209ea4b3f98dcc870c6a71108368482c
IlhamCeferov <ceferov.ilham@gmail.com>
2023-04-20 16:44:41 +0400

    First commit

---
+++ src/main/java/az/ilham/happyminifamily/HappyMiniFamilyApplication.java
@@ -0,0 +1,13 @@
+package az.ilham.happyminifamily;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappyMiniFamilyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HappyMiniFamilyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/az/ilham/happyminifamily/HappyMiniFamilyApplicationTests.java
@@ -0,0 +1,13 @@
+package az.ilham.happyminifamily;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HappyMiniFamilyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
